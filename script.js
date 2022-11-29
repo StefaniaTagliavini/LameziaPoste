c43627099660b95401b9dc257397bd74d6d7f657
Effy <St.tagliavini@gmail.com>
2022-11-29 17:33:01 +0100

    @media aggiustati e scritto javascript per il menu

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ plus.onclick = () =>{
     socialList.classList.toggle('active');
 }
 
+
+// open navmenu // 
+
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close navmenu// 
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display="none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
